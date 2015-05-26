67c4f8f4f1ed760d99930c39b98f2c352a0de0eb
mohitsethi <mohit@sethis.in>
2015-05-26 10:49:50 +0530

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Mohit Sethi"
+maintainer_email "mohit@sethis.in"
+license          "Apache License, Version 2.0"
+description      "Installs/Configures vsftpd for Secure SSL SFTP"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
